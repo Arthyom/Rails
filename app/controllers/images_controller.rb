abe76b03636706ecbe96af55a3aa16adc5a00509
arthyom <a.gonzalezgavina@ugto.mx>
2019-01-09 01:47:04 -0600

    un update

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,13 +1,29 @@
 class ImagesController < ApplicationController
     def index
+        @images = Image.all 
     end
 
     def new 
+        @image = Image.new
+    end
+
+    def edit
+        @image = Image.find(params[:id])
+    end
+
+    def update
+        @image = Image.find params[:id]
+        @image.update = image_params
+        redirect_to @image
+    end
+    def show
+        @image = Image.find params[:id]
     end
 
     def create
         @image = Image.new image_params
         @image.save
+        redirect_to @image
     end
 
     private
